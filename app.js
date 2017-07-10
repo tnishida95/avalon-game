a8dfb6d17e95c73a4e685b54e698c736756b362b
tnishida95 <tnishida95@gmail.com>
2017-07-09 17:47:30 -0700

    Added some socket communication

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+//app.js
+
 console.log("Hello World");
+
+//Express
+/*
+creates a serv and makes it listen on the port 2000
+by default, domain is localhost
+	go to "localhost:2000" in browser to test
+	make sure "node app.js" has been called from the command line
+*/
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+//End Express
+
+//Socket.io
+var io = require('socket.io')(serv,{});
+
+//whenever there is a connection, print 'socket connection'
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');
+
+	socket.on('happy',function(data){
+		console.log('happy because ' + data.reason);
+	});
+
+	socket.emit('serverMsg',{
+		msg:'hello',
+	});
+
+});
