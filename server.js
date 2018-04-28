70d801b868d78d92c460deb6cb372c4389e3c0ea
Tyler Nishida <tnishida95@gmail.com>
2018-04-28 13:34:32 -0700

    Changed port settings

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ Tyler Nishida, tnishida95@gmail.com
 
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 2000;
+
 var serv = require('http').Server(app);
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 app.use('/',express.static(__dirname + '/'));
-serv.listen(2000);
+serv.listen(port);
 console.log("Server started listening on port 2000.");
 var gameStrBuilder = require('./gameStrBuilder');
 
